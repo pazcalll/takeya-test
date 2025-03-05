e99af3ee5db0ab68c7e9244944dae29a7d118cb6
pazcalll <yazeedarifin@gmail.com>
2025-03-05 22:01:35 +0700

    feat: seeders and factories

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::factory()
+            ->count(12)
+            ->has(Post::factory()->count(4))
+            ->create();
+    }
+}
